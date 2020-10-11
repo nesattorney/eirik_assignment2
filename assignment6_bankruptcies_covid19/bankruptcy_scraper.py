ecee5ea869e48162060d566f5f843e6e0848e07f
Eirik Engmo Pettersen <epe083@uit.no>
2020-10-11 17:26:57 +0200

    wip assignment 6

--- assignment6_bankruptcies_covid19/bankruptcy_scraper.py
+++ assignment6_bankruptcies_covid19/bankruptcy_scraper.py
@@ -1,39 +1,38 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
+from bs4 import BeautifulSoup
+import requests
+import csv
+import time
 import pandas as pd
 
-class CoursesSpider(scrapy.Spider):
-    name = "courses"
-    start_urls = [
-                "https://w2.brreg.no/kunngjoring/kombisok.jsp?datoFra=01.01.2019&datoTil=01.02.2019&id_region=0&id_niva1=51&id_niva2=56&id_bransje1=0",
-    ]
+def cleantext(text):
+    return text.get_text().strip("\n").replace("\xa0", "").split("\n")
 
-    def parse(self, response):
-        date_from = response.url.split("&")[1]
-        date_to = response.url.split("&")[2]
-        course = course[:-7]
- 
-        filename = date_from + date_to
+url = "https://w2.brreg.no/kunngjoring/kombisok.jsp?datoFra=01.01.2019&datoTil=02.01.2019&id_region=0&id_niva1=51&id_niva2=56&id_bransje1=0"
 
-        # Count number of bankruptcies for the given month
-        i = 0
-        for field in response.css('tr[bgcolor="#FFFFFF"] td>p>a::text'):
-            i += 1
-        with open(filename, 'w') as f:
-            f.write(str(i))
-        self.log('Saved file %s' % filename)
+body = requests.get(url).content
 
-process = CrawlerProcess(settings={
-    "FEEDS": {
-        "items.json": {"format": "json"},
-    },
-})
+soup = BeautifulSoup(body, 'html.parser')
 
-process.crawl(CoursesSpider)
-process.start()
+myTable = soup.find_all("table", style="border-color: #999999;")
+trs = myTable[0].find_all("tr")
 
-df = pd.read_csv("dates", names=["Date", "Time", "Course", "Description", "Teacher"])
+# for entry in trs:
+#     split = cleantext(entry)
+#     print(split)
 
-df.info()
+cleanlist = [cleantext(x) for x in trs]
 
-print(df)
+# print(cleanlist)
+
+strippedlist = [item for item in cleanlist if len(item) == 12]
+
+# print(strippedlist)
+relevantFields = [1, 4, 6, 8]
+finishedlist = [[row[i] for i in relevantFields] for row in strippedlist]
+
+finishedliststripped = [[element.strip() for element in row] for row in finishedlist]
+
+print(finishedliststripped)
+# cleaned = [x.strip("\n").replace("\xa0", "").split("\n") for x in trs]
+
+# print(cleaned)
